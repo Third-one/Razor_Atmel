94c9972523013f79713afeceb59238e6bcd1fef0
JayZhang <571844355@qq.com>
2019-11-17 12:18:51 +0800

    11.17

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -136,10 +136,30 @@ State Machine Function Definitions
 /* Wait for ??? */
 static void UserApp1SM_Idle(void)
 {
-
+  static u16 u16LedCount=0;
+  static u16 u16LedDelay=0;
+  static u16 u16LedMax=500;
+  static u8  u8LedState=0;
+  u16LedCount++;
+  if(u16LedCount>=3000){
+    u16LedCount=0;
+    u16LedMax/=2;
+    u16LedDelay=0;
+    return;
+  }
+  u16LedDelay++;
+  if(u16LedDelay>=u16LedMax){
+    u16LedDelay=0;
+    if(u8LedState==0){
+      u8LedState=1;
+      LedOn(RED);
+    }else{
+      u8LedState=0;
+      LedOff(RED);
+    }
 } /* end UserApp1SM_Idle() */
     
-
+}
 /*-------------------------------------------------------------------------------------------------------------------*/
 /* Handle an error */
 static void UserApp1SM_Error(void)          
